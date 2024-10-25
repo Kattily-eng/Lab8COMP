c2b8ce5a2745bafe5095667a63baa89c103f5310
Kattily-eng <113124949+Kattily-eng@users.noreply.github.com>
2024-10-24 20:53:24 -0600

    Update CustomListTest.java

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -7,10 +7,49 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 //import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static
+androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.anything;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;import java.util.ArrayList;
 
 public class CustomListTest {
-
-
+@Rule
+public ActivityScenarioRule<MainActivity> scenario = new
+ActivityScenarioRule<MainActivity>(MainActivity.class);    
+  @Test
+    public void TesthasCities(){
+      City A=new City("Ed","AB");
+      cityDataList.add( A);  
+      assertTrue(CustomList.hascity(A));      
+    }
+  
+  public void TestdeleteCities(){
+      City A=new City("Ed","AB");
+      cityDataList.add(A);  
+      CityDataList.delete(A)
+      assertFalse(CustomList.hascity(A));   
+    }
+  public void TestcountCities(){
+     assertEquals(6,CustomList.countcity(A));  
+    City A=new City("Edmonton","AB");
+      cityDataList.add(A);  
+      
+      assertEquals(7,CustomList.countcity(A));   
+    }  
 }
